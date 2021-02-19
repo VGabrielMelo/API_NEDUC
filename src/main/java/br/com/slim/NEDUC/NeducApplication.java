a497dcaa597e71ffa8f0d6a2a88324c17da3fff6
Victor Melo <victor.845.melo@gmail.com>
2021-02-18 21:50:12 -0300

    Criação do projeto Spring Boot

---
+++ src/main/java/br/com/slim/NEDUC/NeducApplication.java
@@ -0,0 +1,13 @@
+package br.com.slim.NEDUC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeducApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeducApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/slim/NEDUC/NeducApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.slim.NEDUC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeducApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
